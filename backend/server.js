b74280165198e3608b0ed0b71c601e19ecc11c09
unknown <shrisohanpal@gmail.com>
2024-07-24 22:01:53 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -18,11 +19,24 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie Parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
-
 app.use('/api/users', userRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+
+    const __dirname = path.resolve();
+
+    // set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    // any route that is not api will be redirected to index.html
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+} else{
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
